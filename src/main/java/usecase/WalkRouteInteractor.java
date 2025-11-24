c7fc4937c161670d5c86e90fc3d9af2e2e8ba8ac
Kyle Hu <khht2006@gmail.com>
2025-11-24 09:58:59 -0500

    walk route

--- src/main/java/usecase/WalkRouteInteractor.java
+++ src/main/java/usecase/WalkRouteInteractor.java
@@ -1,66 +1,37 @@
 package usecase;
 
-import entity.Location;
-import use_case.GetCurrentLocationUseCase;
-import use_case.GetDestinationUseCase;
-import api.fetcher.APIFetcher;
+import app.ApiFetcher;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class WalkRouteInteractor {
 
-    private final GetCurrentLocationUseCase currentLocationUseCase;
-    private final GetDestinationUseCase destinationUseCase;
-    private final APIFetcher apiFetcher;
-    private final String orsApiKey;
+    private final ApiFetcher apiFetcher;
 
-    public WalkRouteInteractor(GetCurrentLocationUseCase currentLocationUseCase,
-                               GetDestinationUseCase destinationUseCase,
-                               APIFetcher apiFetcher,
-                               String orsApiKey) {
-        this.currentLocationUseCase = currentLocationUseCase;
-        this.destinationUseCase = destinationUseCase;
+    public WalkRouteInteractor(ApiFetcher apiFetcher) {
         this.apiFetcher = apiFetcher;
-        this.orsApiKey = orsApiKey;
     }
 
-    public WalkRouteResponse execute() throws Exception {
+    public WalkRouteResponse execute(double startLat, double startLng,
+                                     double endLat, double endLng) throws Exception {
 
-        // Retrieve coordinates
-        Location start = currentLocationUseCase.getLocation();
-        Location end = destinationUseCase.getDestinationLocation();
+        // Call existing API method (lon, lat order!)
+        String jsonString = apiFetcher.fetchWalkingDirectionsJson(
+                startLng, startLat, endLng, endLat);
 
-        // ORS Directions Endpoint
-        String url = "https://api.openrouteservice.org/v2/directions/foot-walking/json";
+        JSONObject responseJson = new JSONObject(jsonString);
 
-        // JSON request body
-        JSONObject body = new JSONObject();
-        JSONArray coords = new JSONArray();
-        coords.put(new JSONArray().put(start.getLongitude()).put(start.getLatitude()));
-        coords.put(new JSONArray().put(end.getLongitude()).put(end.getLatitude()));
-        body.put("coordinates", coords);
-
-
-
-        // Make request
-        JSONObject responseJson = apiFetcher.postJson(url, body.toString(), orsApiKey);
-
-        // Parse response
         JSONObject summary = responseJson
                 .getJSONArray("routes")
                 .getJSONObject(0)
                 .getJSONObject("summary");
 
-        double distanceMeters = summary.getDouble("distance"); // meters
-        double durationSeconds = summary.getDouble("duration"); // seconds
-
-        double distanceKm = distanceMeters / 1000.0;
-        double timeMinutes = durationSeconds / 60.0;
+        double distanceKm = summary.getDouble("distance") / 1000.0;
+        double timeMinutes = summary.getDouble("duration") / 60.0;
 
         return new WalkRouteResponse(distanceKm, timeMinutes);
     }
 
-    // DTO response
     public static class WalkRouteResponse {
         public final double distanceKm;
         public final double timeMinutes;
