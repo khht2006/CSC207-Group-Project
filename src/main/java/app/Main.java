50cb0ac87f2f54681d526672fa89d93b194ebe09
celinec41 <celinesy.chen@mail.utoronto.ca>
2025-11-23 23:17:11 -0500

    Add search history feature and UI

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,5 +1,11 @@
 package app;
 
+import usecase.get_bike_cost.GetBikeCostInputData;
+import usecase.search_history.SearchHistoryData;
+import usecase.search_history.SearchRecord;
+import interface_adapter.SearchHistoryGateway;
+import view.SearchHistoryPanel;
+
 import interface_adapter.GetBikeTimeController;
 import interface_adapter.GetBikeTimePresenter;
 import interface_adapter.GetBikeTimeViewModel;
@@ -25,44 +31,72 @@ public class Main {
         SwingUtilities.invokeLater(Main::createAndShowUI);
     }
 
-    private static double extractMinutes(String text) {
-        String num = text.replaceAll("[^0-9.]", "");
-        return Double.parseDouble(num);
-    }
-
     private static void createAndShowUI() {
+        // Delete the search history when exit the app
+        new java.io.File("search_history.txt").delete();
+
         JFrame frame = new JFrame("Grapes Trip Planner");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         ApiFetcher apiFetcher = new ApiFetcher();
         GeocodeLocationInteractor geocodeInteractor = new GeocodeLocationInteractor(apiFetcher);
 
+        SearchHistoryData historyGateway = new SearchHistoryGateway();
+
+        // Bike time
         GetBikeTimeViewModel bikeTimeViewModel = new GetBikeTimeViewModel();
         GetBikeTimePresenter bikeTimePresenter = new GetBikeTimePresenter(bikeTimeViewModel);
         BikeRouteInteractor bikeRouteInteractor = new BikeRouteInteractor(apiFetcher, bikeTimePresenter);
         GetBikeTimeController bikeTimeController = new GetBikeTimeController(bikeRouteInteractor);
         GetTimePanel bikeTimePanel = new GetTimePanel(bikeTimeViewModel, bikeTimeController);
 
+        // Bike cost
         GetBikeCostViewModel bikeCostViewModel = new GetBikeCostViewModel();
         GetBikeCostPresenter bikeCostPresenter = new GetBikeCostPresenter(bikeCostViewModel);
         GetBikeCostInteractor bikeCostInteractor = new GetBikeCostInteractor(bikeCostPresenter);
-        GetBikeCostController bikeCostController = new GetBikeCostController(bikeCostInteractor,bikeTimeViewModel);
+        GetBikeCostController bikeCostController =
+                new GetBikeCostController(bikeCostInteractor, bikeTimeViewModel);
         GetCostPanel bikeCostPanel = new GetCostPanel(bikeCostViewModel);
 
+        // Origin + history panels
         OriginalDestinationPanel originDestPanel = new OriginalDestinationPanel();
+        SearchHistoryPanel historyPanel = new SearchHistoryPanel();
 
         CardLayout layout = new CardLayout();
         JPanel root = new JPanel(layout);
         root.add(originDestPanel, "origin");
         root.add(bikeTimePanel, "bikeTime");
         root.add(bikeCostPanel, "bikeCost");
+        root.add(historyPanel, "searchHistory");
 
         new OriginalDestinationController(originDestPanel, geocodeInteractor, (origin, destination) -> {
             layout.show(root, "bikeTime");
             bikeTimePanel.requestBikeTime(
                     origin.getLatitude(), origin.getLongitude(),
-                    destination.getLatitude(), destination.getLongitude());
+                    destination.getLatitude(), destination.getLongitude()
+            );
             bikeTimePanel.updateBikeTimeText();
+            // (you can add saving history here later)
+            // Save History
+            double bikeTime = bikeTimeViewModel.getBikeTimeValue();
+
+            GetBikeCostInputData costInput = new GetBikeCostInputData(bikeTime);
+            bikeCostInteractor.execute(costInput);
+            double bikeCost = bikeCostViewModel.getBikeCostValue();
+
+            //Need to change after walking usecase is done
+            double walkTime = bikeTime;
+
+            SearchRecord record = new SearchRecord(
+                    origin.getName(),
+                    destination.getName(),
+                    bikeTime,
+                    bikeCost,
+                    walkTime
+            );
+
+            historyGateway.save(record);
+
         });
 
         // from Time to Cost (See Bike Cost)
@@ -80,6 +114,22 @@ public class Main {
         // From Cost to Time (Back)
         bikeCostPanel.getBackButton().addActionListener(e -> {
             layout.show(root, "bikeTime");
+        });
+
+        // View Search History (button on origin panel)
+        originDestPanel.getViewHistoryButton().addActionListener(e -> {
+            var records = historyGateway.load();
+            if (records.isEmpty()) {
+                historyPanel.setNoHistoryMessage();
+            } else {
+                historyPanel.setHistory(records);
+            }
+            layout.show(root, "searchHistory");
+        });
+
+        // From Search History to Original (Back)
+        historyPanel.getBackButton().addActionListener(e -> {
+            layout.show(root, "origin");
         });
 
         frame.getContentPane().add(root, BorderLayout.CENTER);
